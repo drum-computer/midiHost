45128da1dd279eb921791e1b4d2b03724ed1a5ef
drum-computer <densi.rhythm@gmail.com>
2020-06-29 18:42:49 +0300

    v.0.1. minimum functionality working

--- LCD_MIDI.ino
+++ LCD_MIDI.ino
@@ -20,31 +20,33 @@ int last_state_down = HIGH;
 
 
 // midi
-int channel = 1;
-int cc = 7;
-int mode = 0; // 0 = cc, 1 = nrpn
+int channel =   1;
+int cc =        7;
+int mode =      0; // 0 = cc, 1 = nrpn
+
+// cursor
+const int cursor_positions[2] = {7, 12};
+int cursor_position = 0;
 
 
-void setup() {
+void setup() 
+{
   initPins();
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
+  initLCD();
+
   // begin midi communication
   Serial.begin(31250);
-
-  // placeholder text
-  lcd.print("Src: Pot1 V:");
-  lcd.setCursor(0, 1);
-  lcd.print("Dst: ch13 cc123");
 }
 
-void loop() {
+
+void loop() 
+{
   // init all sensors
-  button_left = digitalRead(LEFT_BUTTON_PIN);
-  button_right = digitalRead(RIGHT_BUTTON_PIN);
-  button_up = digitalRead(UP_BUTTON_PIN);
-  button_down = digitalRead(DOWN_BUTTON_PIN);
-  pot1_val = map(analogRead(POT1_PIN), 0, 1023, 0, 127);
+  button_left =   digitalRead(LEFT_BUTTON_PIN);
+  button_right =  digitalRead(RIGHT_BUTTON_PIN);
+  button_up =     digitalRead(UP_BUTTON_PIN);
+  button_down =   digitalRead(DOWN_BUTTON_PIN);
+  pot1_val =      map(analogRead(POT1_PIN), 0, 1023, 0, 127);
 
   // check all sensors in the loop
   // check potentiometer
@@ -57,7 +59,7 @@ void loop() {
   // check left button
   if(button_left == LOW && last_state_left == HIGH)
   {
-    // leftPress();
+    moveLeft();
     last_state_left = LOW;
   }
   if(button_left == HIGH && last_state_left == LOW)
@@ -66,6 +68,7 @@ void loop() {
   // check right button
   if(button_right == LOW && last_state_right == HIGH)
   {
+    moveRight();
     last_state_right = LOW;
   }
   if(button_right == HIGH && last_state_right == LOW)
@@ -92,18 +95,44 @@ void loop() {
   delay(10);
 }
 
-void initPins()
+void moveLeft()
 {
-  pinMode(POT1_PIN, INPUT);
-  pinMode(LEFT_BUTTON_PIN, INPUT);
-  pinMode(RIGHT_BUTTON_PIN, INPUT);
-  pinMode(UP_BUTTON_PIN, INPUT);
-  pinMode(DOWN_BUTTON_PIN, INPUT);
+  cursor_position = !cursor_position;
+  lcd.setCursor(cursor_positions[cursor_position], 1);
 }
 
-void leftPress()
+void moveRight()
 {
+  cursor_position = !cursor_position;
+  lcd.setCursor(cursor_positions[cursor_position], 1);
+}
 
+void valueUp()
+{
+  switch (cursor_position)
+  {
+  case 0:
+    channelUp();
+    break;
+  
+  case 1:
+    ccUp();
+    break;
+  }
+}
+
+void valueDown()
+{
+  switch (cursor_position)
+  {
+  case 0:
+    channelDown();
+    break;
+  
+  case 1:
+    ccDown();
+    break;
+  }
 }
 
 void ccUp()
@@ -113,6 +142,7 @@ void ccUp()
   cc = (cc == 127) ? 0 : cc + 1;
   lcd.setCursor(12, 1);
   lcd.print(cc);
+  lcd.setCursor(cursor_positions[cursor_position], 1);
 }
 
 void ccDown()
@@ -122,6 +152,7 @@ void ccDown()
   cc = (cc == 0) ? 127 : cc - 1;
   lcd.setCursor(12, 1);
   lcd.print(cc);
+  lcd.setCursor(cursor_positions[cursor_position], 1);
 }
 
 void channelUp()
@@ -131,6 +162,7 @@ void channelUp()
   channel = (channel == 16) ? 1 : channel + 1;
   lcd.setCursor(7, 1);
   lcd.print(channel);
+  lcd.setCursor(cursor_positions[cursor_position], 1);
 }
 
 void channelDown()
@@ -140,7 +172,9 @@ void channelDown()
   channel = (channel == 1) ? 16 : channel - 1;
   lcd.setCursor(7, 1);
   lcd.print(channel);
+  lcd.setCursor(cursor_positions[cursor_position], 1);
 }
+
 void sendMidiCC(int val)
 {
   Serial.write(176 + (channel - 1));
@@ -151,4 +185,29 @@ void sendMidiCC(int val)
   lcd.print("   ");
   lcd.setCursor(12, 0);
   lcd.print(pot1_val);
+}
+
+// void valueUp()
+// {
+//   if(cursor_position = )
+// }
+
+void initPins()
+{
+  pinMode(POT1_PIN, INPUT);
+  pinMode(LEFT_BUTTON_PIN, INPUT);
+  pinMode(RIGHT_BUTTON_PIN, INPUT);
+  pinMode(UP_BUTTON_PIN, INPUT);
+  pinMode(DOWN_BUTTON_PIN, INPUT);
+}
+
+void initLCD()
+{
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Src: Pot1 V:");
+  lcd.setCursor(0, 1);
+  lcd.print("Dst: ch13 cc123");
+  lcd.setCursor(7, 1);
+  lcd.cursor();
 }
