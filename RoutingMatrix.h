dd65b9395f10601f18c563e03e6b8ccb83412f45
drum-computer <densi.rhythm@gmail.com>
2020-07-30 21:44:37 +0300

    Simplify routing matrix. Put all project-specific code to main .ino file

--- RoutingMatrix.h
+++ RoutingMatrix.h
@@ -16,15 +16,6 @@ public:
       matrix[i] = i;
   }
 
-  void route(byte input_midi_channel, byte input_cc, 
-              byte* output_midi_channel, byte* output_cc)
-  {
-    int index = (input_midi_channel * Constants::NUM_CONTROLLERS) + input_cc;
-    *output_midi_channel = matrix[index] / Constants::NUM_CONTROLLERS;
-    *output_cc = matrix[index] % Constants::NUM_CONTROLLERS;
-
-  }
-
   int getDestination(int address)
   {
     return matrix[address];
